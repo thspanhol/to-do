a19c2eadc639f971c4544c9ac806d9652d897ba1
Thales Spanhol <thales_spanhol@hotmail.com>
2023-03-22 22:45:03 -0300

    Feat: leitura do firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+
 
 function App() {
+
+  const [todos, setTodos] = useState("");
+
+  //Create
+  //Read
+  useEffect(() => {
+    const q = query(collection(db, 'todos'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id: doc.id})
+      });
+      console.log(todosArr);
+      setTodos(todosArr);
+    })
+    return () => unsubscribe;
+  }, [])
+  //Update
+  //Delete
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Spanhol</h1>
+      <input type="text"/>
+      <button>Enviar</button>
+      {todos !== "" && todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <h4>{todo.text}</h4>
+            <input type="checkbox" checked={todo.completed}/>
+          </div>
+        )
+      })}
     </div>
   );
 }
