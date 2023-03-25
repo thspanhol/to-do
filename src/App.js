14c7e4d5d6812f74f27fc3066d8ece1dff77f803
Thales Spanhol <thales_spanhol@hotmail.com>
2023-03-24 22:54:32 -0300

    Feat: funções de criar e excluir tarefas feitas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { collection, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "./firebase";
 
@@ -6,8 +6,21 @@ import { db } from "./firebase";
 function App() {
 
   const [todos, setTodos] = useState("");
+  const [quest, setQuest] = useState("");
 
   //Create
+  const createTodo = async () => {
+    if(quest === "") {
+      alert('Primeiro escreva sua tarefa.')
+      return
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: quest,
+      completed: false,
+      id: "teste1"
+    })
+    setQuest('');
+  }
   //Read
   useEffect(() => {
     const q = query(collection(db, 'todos'));
@@ -28,18 +41,22 @@ function App() {
     });
   };
   //Delete
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id))
+  };
 
 
   return (
     <div>
       <h1>Spanhol</h1>
-      <input type="text"/>
-      <button>Enviar</button>
+      <input type="text" onChange={(e) => setQuest(e.target.value)} value={quest}/>
+      <button onClick={() => createTodo()}>Enviar</button>
       {todos !== "" && todos.map((todo) => {
         return (
           <div key={todo.id}>
             <h4 onClick={() => toggleComplete(todo)}>{todo.text}</h4>
             <input type="checkbox" checked={todo.completed}/>
+            <button onClick={() => deleteTodo(todo.id)}>Excluir</button>
           </div>
         )
       })}
